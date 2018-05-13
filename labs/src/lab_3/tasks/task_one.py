208d960c3da1df1873045b9e1603fcbf5d3b05ac
AndreyBelousow <groundhog448@gmail.com>
2018-05-13 13:47:15 +0400

    kek

--- labs/src/lab_3/tasks/task_one.py
+++ labs/src/lab_3/tasks/task_one.py
@@ -1,25 +1,67 @@
 import numpy as np
 import math as m
+from matplotlib import pyplot as pl
 
 
 def f(x):
-    return m.exp(3 * x) / x ** 3 + m.sqrt(x + 1)
+    return m.exp(3 * x) / ((x ** 3) + m.sqrt(x + 1))
+
+
+def source_function_plot(a, b, n):
+    pl.figure("Source function")
+
+    x = np.linspace(a, b, n)
+    y = []
+    step = (b - a) / n
+
+    for i in range(n):
+        y.append(f(a + i * step))
+
+    pl.plot(x, y, 'purple', label='source function')
+    pl.legend()
+    pl.grid()
+    pl.show()
 
 
 def trapezoidal_integration(a, b, n):
-    h = (b-a)/n
-    s = 0.5*(f(a)+f(b))
-    for i in range(1, n, 1):
-        s = s + f(a + i * h)
-    return h * s
+    h = (b - a) / n
+    res = 0.5 * (f(a) + f(b))
+    for i in range(1, n):
+        res += f(a + i * h)
+    return h * res
+
+
+def trapezoidal_integration_with_precision(a, b, eps):
+    n = 0
+    prev_res = float('inf')
+    res = 0
+    while abs(prev_res - res) > eps:
+        n += 1
+        prev_res = res
+        res = trapezoidal_integration(a, b, n)
+    # print('Trap. integration for eps=', eps, 'completed with', n, 'iterations')
+    return res
+
+
+def simpson_integration(a, b, n):
+    h = (b - a) / n
+    res = f(a) + f(b)
+    for i in range(0, n, 2):
+        res += 2 * f(a + i * h)
+    for i in range(1, n, 2):
+        res += 4 * f(a + i * h)
+    return h * res / 3
+
 
 def run():
     a = 2
     b = 3
     eps = 10 ** -5
-    n = 100
+    n = 10
 
-    print(trapezoidal_integration(a, b, n))
+    source_function_plot(a, b, n)
+    print('Trap. result:', trapezoidal_integration_with_precision(a, b, eps))
+    print('Simpson result:', simpson_integration(a, b, n))
 
 
 if __name__ == '__main__':
